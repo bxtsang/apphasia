72fd91413c02b768dc21ddca95b3b138a81c86cb
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2020-12-23 01:44:52 +0800

    Update index.vue

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -7,7 +7,7 @@
       </div>
       <v-card>
         <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template 👨
+          Welcome to the Vuetify + Nuxt.js template 👨♥
         </v-card-title>
         <v-card-text>
           <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
