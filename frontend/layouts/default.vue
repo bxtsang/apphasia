f9717978279cf5165809ff895f6db0572129ea56
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2020-12-27 02:36:47 +0800

    default template for logged in pages

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,116 +1,123 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
+  <v-app :style="{background: $vuetify.theme.themes[theme].background}">
+    <v-navigation-drawer fixed app permanent>
+      <v-img src="/asg.png"/>
       <v-list>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="title">
+              Hello, Admin
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <v-divider />
+      <v-list nav dense>
+        <v-subheader class="py-0">
+          General
+        </v-subheader>
         <v-list-item
           v-for="(item, i) in items"
           :key="i"
           :to="item.to"
+          color="primary"
+          link
           router
-          exact
         >
-          <v-list-item-action>
+          <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
+          </v-list-item-icon>
           <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
-    </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
-    <v-main>
-      <v-container>
-        <nuxt />
-      </v-container>
-    </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+      <v-divider />
+      <v-list dense nav>
+        <v-subheader class="py-0">
+          Settings
+        </v-subheader>
+        <v-list-item
+          v-for="(item, i) in settings"
+          :key="i"
+          :to="item.to"
+          color="primary"
+          link
+          router
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
         </v-list-item>
       </v-list>
+      <template v-slot:append>
+        <v-list dense nav>
+          <v-list-item link to="/login">
+            <v-list-item-icon>
+              <v-icon>{{ 'mdi-logout' }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Logout</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </template>
     </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
+    <v-main>
+      <div class="pa-6">
+        <nuxt />
+      </div>
+    </v-main>
   </v-app>
 </template>
 
 <script>
 export default {
+  // middleware: ['isLoggedIn'],
   data () {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
       items: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
+          icon: 'mdi-view-dashboard',
+          title: 'Dashboard',
           to: '/'
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
+          icon: 'mdi-calendar-check',
+          title: 'Projects',
+          to: '/projects'
+        },
+        {
+          icon: 'mdi-clipboard-account',
+          title: 'Manage PWAs',
+          to: '/pwa'
+        },
+        {
+          icon: 'mdi-account-group',
+          title: 'Manage Staff',
+          to: '/staff'
         }
       ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      settings: [
+        {
+          icon: 'mdi-cog',
+          title: 'Settings',
+          to: '/settings'
+        },
+        {
+          icon: 'mdi-bell',
+          title: 'Notifications',
+          to: '/notifications'
+        }
+      ]
+    }
+  },
+  computed: {
+    theme () {
+      return (this.$vuetify.theme.dark) ? 'dark' : 'light'
     }
   }
 }
