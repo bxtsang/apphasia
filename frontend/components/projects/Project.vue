2dbad8024869dbacef20e1575ab4be2a18aa2f36
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2020-12-29 04:36:24 +0800

    project page template

---
+++ frontend/components/projects/Project.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-card class="pa-6">
+    <h1 class="title">{{ project.name }}</h1>
+  </v-card>
+</template>
+<script>
+export default {
+  props: { project: Object }
+}
+</script>
---
+++ frontend/pages/projects.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <v-row>
+      <v-col>
+        <v-btn color="primary">
+          Add New Project
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-card class="px-6 py-3">
+          <h1 class="title">Projects</h1>
+          <v-row>
+            <v-col v-if="isLoading" class="d-flex justify-center">
+              <v-progress-circular
+                :size="50"
+                color="primary"
+                indeterminate
+              ></v-progress-circular>
+            </v-col>
+            <v-col v-else v-for="(project, index) in projects" :key="index" cols="6">
+              <Project :project="project"></Project>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+<script>
+import Project from '../components/projects/Project.vue'
+
+export default {
+  components: { Project },
+  data () {
+    return {
+      isLoading: true,
+      projects: []
+    }
+  },
+  mounted () {
+    this.getProjects()
+  },
+  methods: {
+    getProjects () {
+      this.projects = [
+        { name: 'Chit-Chat', task: 2 },
+        { name: 'Aphasia Choir', task: 3 }
+      ]
+      this.isLoading = false
+    }
+  }
+}
+</script>
