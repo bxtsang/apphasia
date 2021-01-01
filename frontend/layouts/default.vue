3e45b696097fcb75c01044380faa623c378b598b
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2021-01-02 05:28:59 +0800

    layout template retrieve user name from auth module

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -6,7 +6,7 @@
         <v-list-item>
           <v-list-item-content>
             <v-list-item-title class="title">
-              Hello, Admin
+              Hello, {{ $auth.user.name }}
             </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
