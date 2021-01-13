a49927b11cc36890ac79751912614cdfb7a5b0c2
arixpsy <42609059+arixpsy@users.noreply.github.com>
2021-01-13 17:03:57 +0800

    create individual staff page template

---
+++ frontend/components/staff/IndividualStaffView.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-row>
+    <v-col>
+      <v-card class="px-6 py-3">
+        <h1 class="title hover-underline">
+          <NuxtLink to="/staff">
+            Staff
+          </NuxtLink> / {{ project ? project.name : "" }}
+        </h1>
+        <v-row>
+          Hello
+        </v-row>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+<script>
+
+export default {
+  data () {
+    return {
+      isLoading: true,
+      staffId: this.$route.query.id
+    }
+  }
+}
+</script>
+<style scoped>
+.hover-underline > a {
+  color: inherit;
+  text-decoration: none !important;
+}
+.hover-underline > a:hover {
+  text-decoration: underline !important;
+}
+</style>
