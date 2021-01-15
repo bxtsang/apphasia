99e7c2e4dc61428467a6404c4b89f5dd0c195a40
arixpsy <42609059+arixpsy@users.noreply.github.com>
2021-01-15 22:29:10 +0800

    Update NewStaffModal.vue

--- frontend/components/staff/modals/NewStaffModal.vue
+++ frontend/components/staff/modals/NewStaffModal.vue
@@ -1,7 +1,7 @@
 <template>
   <v-dialog
     v-model="isOpen"
-    width="700"
+    width="800"
   >
     <template v-slot:activator="{ on, attrs }">
       <v-btn color="primary" v-bind="attrs" v-on="on">
@@ -16,12 +16,55 @@
           <v-radio v-for="role in ROLE_OPTIONS" :key="role.value" :label="role.label" :value="role.value" />
         </v-radio-group>
         <v-subtitle>Personal Details</v-subtitle>
-        <v-text-field
-          v-model="staffData.fullname"
-          :rules="fullnameRules"
-          label="Fullname"
-          required
-        />
+        <v-container class="px-0">
+          <v-row>
+            <v-col>
+              <v-text-field
+                v-model="staffData.fullname"
+                :rules="fullnameRules"
+                label="Full Name"
+                required
+              />
+            </v-col>
+            <v-col>
+              <v-text-field
+                v-model="staffData.nickname"
+                :rules="nicknameRules"
+                label="Nickname / Alias"
+                required
+              />
+            </v-col>
+            <v-col>
+              <v-text-field
+                v-model="staffData.nric"
+                :rules="nricRules"
+                label="NRIC"
+                required
+              />
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <input type="date" />
+            </v-col>
+            <v-col>
+              <v-text-field
+                v-model="staffData.contact_number"
+                :rules="contactRules"
+                label="Contact"
+                required
+              />
+            </v-col>
+            <v-col>
+              <v-text-field
+                v-model="staffData.gender"
+                :rules="nricRules"
+                label="NRIC"
+                required
+              />
+            </v-col>
+          </v-row>
+        </v-container>
         <v-btn color="primary" class="my-3" type="submit">
           Save
         </v-btn>
@@ -36,16 +79,35 @@ export default {
       isOpen: false,
       valid: true,
       ROLE_OPTIONS: [
-        { label: 'Core Team', value: 'coreteam' },
+        { label: 'Core Team', value: 'core_team' },
         { label: 'Intern', value: 'intern' },
-        { label: 'Core Volunteer', value: 'corevolunteer' }
+        { label: 'Core Volunteer', value: 'core_volunteer' }
       ],
       staffData: {
+        role: '',
         fullname: '',
-        role: ''
+        nickname: '',
+        nric: '',
+        dob: '',
+        contact_number: '',
+        gender: '',
+        email: '',
+        home_address: ''
       },
-      fullnameRules: [
-        v => !!v || 'E-mail is required'
+      additionalData: {
+        bio: '',
+        languages: [],
+        work_location: '',
+        profession: '',
+        isST: false,
+        data_joined: '',
+        supervisor: ''
+      },
+      fullnameRules: [v => !!v || 'Fullname is required'],
+      nicknameRules: [v => !!v || 'Nickname is required'],
+      nricRules: [
+        v => !!v || 'NRIC is required',
+        v => /^[STFG]\d{7}[A-Z]$/.test(v) || 'Not a valid NRIC'
       ]
     }
   },
