0c0d178463bee8e589f9430c9e643cf171a2bdb7
Cliffenlee <cliffen.lee.2018@sis.smu.edu.sg>
2021-01-22 09:09:15 +0800

    added access control allow origin header

--- backend/lambda/verifyAttribute/lambda_function.py
+++ backend/lambda/verifyAttribute/lambda_function.py
@@ -30,7 +30,8 @@ def lambda_handler(event, context):
     return {
         "statusCode": statusCode,
         "headers": {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
         },
         "body": json.dumps(result)
     }
