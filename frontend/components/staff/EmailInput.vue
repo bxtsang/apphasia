e2bb67eed902eab235130f2292a52107b555955a
bxtsang <bxtsang.2018@sis.smu.edu.sg>
2021-01-26 16:04:17 +0800

    Create EmailInput component

---
+++ frontend/components/staff/EmailInput.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-text-field
+    v-model="inputValue"></v-text-field>
+</template>
+
+<script>
+export default {
+  name: 'EmailInput',
+
+  props: {
+    inputValue: {
+      type: String,
+      default: ''
+    }
+  },
+
+  watch: {
+    inputValue (newValue, oldValue) {
+      this.$emit('input', newValue)
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
