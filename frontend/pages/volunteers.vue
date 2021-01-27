3308b222f88db0ce3c77abc8a25b83e6b3a77ab3
arixpsy <42609059+arixpsy@users.noreply.github.com>
2021-01-27 19:09:57 +0800

    create volunteer overview page

---
+++ frontend/pages/volunteers.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <IndividualVolunteerView v-if="volunteerId" :resourceType="resourceType"/>
+    <v-container v-else class="pa-0" fluid>
+      <v-row>
+        <v-col>
+          <AddResourceModal :resourceType="resourceType"/>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <ListingQuery :resourceType="resourceType"></ListingQuery>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+<script>
+import AddResourceModal from './../components/modals/AddResourceModal'
+import IndividualVolunteerView from './../components/volunteer/IndividualVolunteerView'
+import ListingQuery from './../components/common/ListingQuery'
+
+export default {
+  components: { ListingQuery, IndividualVolunteerView, AddResourceModal },
+  data () {
+    return {
+      volunteerId: this.$route.query.id,
+      resourceType: 'volunteers'
+    }
+  },
+  watch: {
+    '$route.query.id': {
+      handler () {
+        this.volunteerId = this.$route.query.id
+      }
+    }
+  }
+}
+</script>
