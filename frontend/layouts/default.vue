07a5cb826eed4ed0c4501a1f5c8beddbe304900a
arixpsy <42609059+arixpsy@users.noreply.github.com>
2021-01-27 20:18:13 +0800

    update mange staffs link in navbar

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -107,7 +107,7 @@ export default {
         },
         {
           icon: 'mdi-account-group',
-          title: 'Manage Staff',
+          title: 'Manage Staffs',
           to: '/staffs'
         }
       ],
