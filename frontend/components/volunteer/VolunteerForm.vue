a58e42b30129efae2e6e952f802fd5398359e774
bxtsang <bxtsang.2018@sis.smu.edu.sg>
2021-02-02 01:55:40 +0800

    Create data fields for apollo mutation on VolunteerForm and remove unnecessary mutations in UpdateVol.graphql

--- frontend/components/volunteer/VolunteerForm.vue
+++ frontend/components/volunteer/VolunteerForm.vue
@@ -9,6 +9,8 @@
 
 <script>
 import UpdateVol from './../../graphql/volunteer/UpdateVol.graphql'
+import GetSingleStaff from '~/graphql/staff/GetSingleStaff.graphql'
+import GetAllStaff from '~/graphql/staff/GetAllStaff.graphql'
 
 export default {
   props: {
@@ -30,20 +32,20 @@ export default {
         dob: this.volunteer.dob,
         email: this.volunteer.email,
         gender: this.volunteer.gender,
-        is_active: this.staff.is_active,
-        is_speech_therapist: this.staff.is_speech_therapist,
-        name: this.staff.name,
-        nickname: this.staff.nickname,
-        notes: this.staff.notes,
-        profession: this.staff.profession,
-        rejection_reason: this.staff.rejection_reason,
-        status: this.volunteer.status_description.description,
+        is_active: this.volunteer.is_active,
+        is_speech_therapist: this.volunteer.is_speech_therapist,
+        name: this.volunteer.name,
+        nickname: this.volunteer.nickname,
+        notes: this.volunteer.notes,
+        profession: this.volunteer.profession,
+        rejection_reason: this.volunteer.rejection_reason,
+        rejected_date: this.volunteer.rejected_date,
+        status: this.volunteer.status_description.status,
         ws_place: this.volunteer.ws_place,
-        // projects_approved: this.volunteer.project_vols.filter(item => item.approved),
-        // projects_interested: this.volunteer.project_vols,
-        vol_languages: this.vol_languages,
-        vol_ic: this.vol_ic,
-        vol_voltypes: this.vol_voltypes
+        projects_interested: this.volunteer.project_vols,
+        vol_languages: this.volunteer.vol_languages,
+        vol_ic: this.volunteer.vol_ic,
+        vol_voltypes: this.volunteer.vol_voltypes
       }
     }
   },
@@ -52,19 +54,106 @@ export default {
       if (this.$refs.form.validate()) {
         const languageChanges = this.getLanguageChanges()
         const volIcChanges = this.getVolIcChanges()
+        const volTypeChanges = this.getVolTypeChanges()
         this.$apollo.mutate({
           mutation: UpdateVol,
           variables: {
-
+            address: this.volunteerData.address,
+            bio: this.volunteerData.bio,
+            channel: this.volunteerData.channel,
+            consent: this.volunteerData.consent,
+            contact_num: this.volunteerData.contact_num,
+            dob: this.volunteerData.dob,
+            gender: this.volunteerData.gender,
+            is_active: this.volunteerData.is_active,
+            is_speech_therapist: this.volunteerData.is_speech_therapist,
+            name: this.volunteerData.name,
+            nickname: this.volunteerData.nickname,
+            notes: this.volunteerData.notes,
+            profession: this.volunteerData.profession,
+            rejection_reason: this.volunteerData.rejection_reason,
+            rejection_date: this.volunteerData.rejection_date,
+            status: this.volunteerData.status,
+            ws_place: this.volunteerData.ws_place,
+            languages_to_add: languageChanges.added,
+            languages_to_remove: languageChanges.removed,
+            // projects_to_add: this.volunteerData.projects_to_add,
+            // projects_to_remove: this.volunteerData.projects_to_remove,
+            ic_to_add: volIcChanges.added,
+            ic_to_remove: volIcChanges.removed,
+            voltypes_to_add: this.volunteerData.voltypes_to_add,
+            voltypes_to_remove: this.volunteerData.voltypes_to_remove
           }
         })
       }
     },
     getLanguageChanges () {
-
+      const originalArray = this.volunteer.languages.map(item => item.language)
+      const currentArray = this.volunteerData.languages
+      const added = []
+      const removed = []
+      for (const language of originalArray) {
+        if (!currentArray.find(item => item === language)) {
+          removed.push(language)
+        }
+      }
+      for (const language of currentArray) {
+        if (!originalArray.find(item => item === language)) {
+          added.push({ language, vol_id: this.volunteer.id })
+        }
+      }
+      return { added, removed }
     },
     getVolIcChanges () {
-
+      const originalArray = this.volunteer.vol_ic.map(item => item.ic)
+      const currentArray = this.volunteerData.vol_ic
+      const added = []
+      const removed = []
+      for (const ic of originalArray) {
+        if (!currentArray.find(item => item === ic)) {
+          removed.push(ic)
+        }
+      }
+      for (const ic of currentArray) {
+        if (!originalArray.find(item => item === ic)) {
+          added.push({ ic, vol_id: this.volunteer.id })
+        }
+      }
+      return { added, removed }
+    },
+    getVolTypeChanges () {
+      const originalArray = this.volunteer.vol_voltypes.map(item => item.voltypes)
+      const currentArray = this.volunteerData.vol_voltypes
+      const added = []
+      const removed = []
+      for (const voltype of originalArray) {
+        if (!currentArray.find(item => item === voltype)) {
+          removed.push(voltype)
+        }
+      }
+      for (const voltype of currentArray) {
+        if (!originalArray.find(item => item === voltype)) {
+          added.push({ voltype, vol_id: this.volunteer.id })
+        }
+      }
+      return { added, removed }
+    },
+    getProjectInterestChanges () {
+      const originalArray = this.volunteer.project_vols.map(item => item.project)
+      const currentArray = this.volunteerData.projects_interested
+      const added = []
+      const removed = []
+      for (const project of originalArray) {
+        if (!currentArray.find(item => item === project)) {
+          removed.push(project)
+        }
+      }
+      for (const project of currentArray) {
+        if (!originalArray.find(item => item === project)) {
+          added.push({ project, vol_id: this.volunteer.id })
+        }
+      }
+      return { added, removed }
     }
   }
 }
