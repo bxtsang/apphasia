b211da0a9e153244f192c2aa7d3b6fc59e781a50
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2021-02-01 23:39:41 +0800

    Create pwas page from template

---
+++ frontend/pages/pwas.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <!-- <IndividualPWAView v-if="pwaId" :resourceType="resourceType"/>
+    <v-container v-else class="pa-0" fluid> -->
+    <v-container class="pa-0" fluid>   
+      <v-row>
+        <v-col>
+          <AddResourceModal :resourceType="resourceType"/>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <ListingQuery :resourceType="resourceType"></ListingQuery>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+<script>
+import AddResourceModal from './../components/modals/AddResourceModal'
+// import IndividualPWAView from './../components/volunteer/IndividualPWAView'
+import ListingQuery from './../components/common/ListingQuery'
+
+export default {
+  components: { ListingQuery, AddResourceModal },
+  data () {
+    return {
+      pwaId: this.$route.query.id,
+      resourceType: 'pwas'
+    }
+  },
+  watch: {
+    '$route.query.id': {
+      handler () {
+        this.pwaId = this.$route.query.id
+      }
+    }
+  }
+}
+</script>
