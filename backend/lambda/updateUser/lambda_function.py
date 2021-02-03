d352295945c7d0c30b45bab47f5a45b1c607c59f
CliffenLee <cliffen.lee.2018@sis.smu.edu.sg>
2021-02-04 01:08:18 +0800

    added aws secret for updateUser

--- backend/lambda/updateUser/lambda_function.py
+++ backend/lambda/updateUser/lambda_function.py
@@ -6,9 +6,9 @@ import base64
 from botocore.exceptions import ClientError
 
 client = boto3.client('cognito-idp')
+clientSecret = boto3.client('secretsmanager')
 
-
-def lambda_handler(event, context): 
+def lambda_handler(event, context):
     result = {}
     old_role = ""
     statusCode = 500
@@ -81,7 +81,7 @@ def lambda_handler(event, context):
                 print(e)
                 result['hasura_status'] = "failed"
                 result['hasura_message'] = "failed to send query to hasura"
-                result['hasura_error'] = str(e) 
+                result['hasura_error'] = str(e)
 
     return {
         "statusCode": statusCode,
@@ -94,55 +94,12 @@ def lambda_handler(event, context):
 
 
 def get_secret():
-    secret = ""
-    decoded_binary_secret= ""
     secret_name = "HASURA_ADMIN_SECRET"
-    region_name = "ap-southeast-1"
 
-    # Create a Secrets Manager client
-    session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
+    response = clientSecret.get_secret_value(
+        SecretId = secret_name
     )
 
-    # In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
-    # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-    # We rethrow the exception by default.
+    print(response)
 
-    try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
-    except ClientError as e:
-        if e.response['Error']['Code'] == 'DecryptionFailureException':
-            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
-            # An error occurred on the server side.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InvalidParameterException':
-            # You provided an invalid value for a parameter.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InvalidRequestException':
-            # You provided a parameter value that is not valid for the current state of the resource.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
-            # We can't find the resource that you asked for.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-    else:
-        # Decrypts secret using the associated KMS CMK.
-        # Depending on whether the secret is a string or binary, one of these fields will be populated.
-        if 'SecretString' in get_secret_value_response:
-            secret = get_secret_value_response['SecretString']
-        else:
-            decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary'])
-
-    print("Secret:", secret)
-    print("Binary Secret:", decoded_binary_secret) 
-    return json.loads(secret)  # returns the secret as dictionary
+    return response['SecretString']
