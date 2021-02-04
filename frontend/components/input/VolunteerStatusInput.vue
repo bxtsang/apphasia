cb93b6ad7e17aeb615a23c0cf12ddd4af01a611c
bxtsang <bxtsang.2018@sis.smu.edu.sg>
2021-02-05 02:55:53 +0800

    Create VolunteerStatusInput component

---
+++ frontend/components/input/VolunteerStatusInput.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-select
+    v-model="data"
+    :items="status"
+    label="Status"
+    :rules="validation"
+    :required="required"
+    :readonly="readonly"
+    :disabled="disabled"
+    multiple
+  />
+</template>
+
+<script>
+import gql from 'graphql-tag'
+
+export default {
+  name: 'VolunteerStatusInput',
+  props: {
+    value: {
+      type: Array,
+      default () {
+        return []
+      }
+    },
+    required: {
+      type: Boolean,
+      default: false
+    },
+    readonly: {
+      type: Boolean,
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  data () {
+    return {
+      data: this.value,
+      validation: []
+    }
+  },
+
+  watch: {
+    data: {
+      immediate: true,
+      handler (newValue, oldValue) {
+        this.$emit('input', newValue)
+      }
+    },
+    value: {
+      handler (newValue, oldValue) {
+        this.data = this.value
+      }
+    }
+  },
+
+  apollo: {
+    status: {
+      query () {
+        return gql`query getVoltypes {
+          status {
+            status
+          }
+        }`
+      },
+      update: data => data.voltypes.map((item) => {
+        return { value: item.status, text: item.status }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
