9022e0a656bcd83e89f665ebab803ff12dcb1063
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2021-02-04 19:17:17 +0800

    Create MediaEngagementInput.vue

---
+++ frontend/components/input/MediaEngagementInput.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-text-field
+    v-model="data"
+    :rules="validation"
+    label="Participated in any media projects?"
+    :required="required"
+    :readonly="readonly"
+    :outlined="outlined"
+  />
+</template>
+
+<script>
+export default {
+  name: 'MediaEngagementInput',
+
+  props: {
+    value: {
+      type: String,
+      default: ''
+    },
+    required: {
+      type: Boolean,
+      default: false
+    },
+    readonly: {
+      type: Boolean,
+      default: false
+    },
+    outlined: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  data () {
+    return {
+      data: this.value,
+      validation: []
+    }
+  },
+
+  watch: {
+    data: {
+      immediate: true,
+      handler (newValue, oldValue) {
+        this.$emit('input', newValue)
+      }
+    },
+    value: {
+      handler (newValue, oldValue) {
+        this.data = this.value
+      }
+    }
+  }
+}
+
+</script>
