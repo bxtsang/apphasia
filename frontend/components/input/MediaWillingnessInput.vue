10f2688bb5d22c811f7ed57abb73716ca8adc91d
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2021-02-04 19:17:06 +0800

    add MediaWillingness Input

---
+++ frontend/components/input/MediaWillingnessInput.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-select
+    v-model="data"
+    :items="OPTIONS"
+    :rules="validation"
+    label="Agreeable to speak to Media?"
+    :required="required"
+    :readonly="readonly"
+    :outlined="outlined"
+  />
+</template>
+
+<script>
+
+export default {
+  name: 'MediaWillingnessInput',
+
+  props: {
+    value: {
+      type: Boolean,
+      default: null
+    },
+    required: {
+      type: Boolean,
+      default: false
+    },
+    readonly: {
+      type: Boolean,
+      default: false
+    },
+    outlined: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  data () {
+    return {
+      OPTIONS: [
+        { text: 'Yes', value: true },
+        { text: 'No', value: false }
+      ],
+      data: this.value,
+      validation: []
+    }
+  },
+
+  watch: {
+    data: {
+      immediate: true,
+      handler (newValue, oldValue) {
+        this.$emit('input', newValue)
+      }
+    },
+    value: {
+      handler (newValue, oldValue) {
+        this.data = this.value
+      }
+    }
+  }
+}
+
+</script>
