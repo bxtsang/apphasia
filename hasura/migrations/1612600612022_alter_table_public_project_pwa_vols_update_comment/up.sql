ac10fdad71785e80b33cc402d54a065ec22f4122
Glen See <seeshaojieglen@gmail.com>
2021-02-06 16:56:24 +0800

    Added more project tables, fields & permissions

---
+++ hasura/migrations/1612600612022_alter_table_public_project_pwa_vols_update_comment/up.sql
@@ -0,0 +1,1 @@
+COMMENT ON TABLE "public"."project_pwa_vols" IS E'Volunteers assigned to a PWA for a particular project';
---
+++ hasura/migrations/1612601299478_alter_table_public_projects_add_column_description/up.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "public"."projects" ADD COLUMN "description" text NOT NULL DEFAULT 'My Project';
