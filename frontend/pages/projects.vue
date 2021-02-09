5d8680e515a70a9bb72a28ebc537223709a93785
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2021-02-10 01:57:43 +0800

    replace listing query and resource modal for projects page

--- frontend/pages/projects.vue
+++ frontend/pages/projects.vue
@@ -1,45 +1,31 @@
 <template>
   <div>
-    <IndividualProjectView v-if="projectId" />
+    <IndividualProjectView v-if="projectId" :resourceType="resourceType"/>
     <v-container v-else class="pa-0" fluid>
       <v-row>
         <v-col>
-          <NewProjectModal />
+          <AddResourceModal :resourceType="resourceType"/>
         </v-col>
       </v-row>
       <v-row>
         <v-col>
-          <v-card class="px-6 py-3">
-            <h1 class="title">Projects</h1>
-            <v-row>
-              <v-col v-if="isLoading" class="d-flex justify-center">
-                <v-progress-circular
-                  :size="50"
-                  color="primary"
-                  indeterminate
-                />
-              </v-col>
-              <v-col v-for="(project, index) in projects" v-else :key="index" cols="6">
-                <Project :project="project" />
-              </v-col>
-            </v-row>
-          </v-card>
+          <ListingQuery :resourceType="resourceType"></ListingQuery>
         </v-col>
       </v-row>
     </v-container>
   </div>
 </template>
 <script>
-import Project from '../components/projects/Project'
-import IndividualProjectView from '../components/projects/IndividualProjectView'
-import NewProjectModal from '../components/projects/modals/NewProjectModal'
+import AddResourceModal from './../components/modals/AddResourceModal'
+import IndividualProjectView from './../components/projects/IndividualProjectView'
 
 export default {
-  components: { Project, IndividualProjectView, NewProjectModal },
+  components: { IndividualProjectView, AddResourceModal },
   data () {
     return {
       isLoading: true,
       projectId: this.$route.query.id,
+      resourceType: 'projects',
       projects: []
     }
   },
@@ -47,24 +33,6 @@ export default {
     '$route.query.id': {
       handler () {
         this.projectId = this.$route.query.id
-      }
-    }
-  },
-  mounted () {
-    setTimeout(this.getProjects, 2000)
-    // this.getProjects()
-  },
-  methods: {
-    async getProjects () {
-      this.projects = []
-      this.isLoading = true
-      try {
-        const response = await this.$axios.get(`${process.env.BASE_API_URL}/projects`)
-        this.projects = response.data
-      } catch (error) {
-        this.projects = []
-      } finally {
-        this.isLoading = false
       }
     }
   }
