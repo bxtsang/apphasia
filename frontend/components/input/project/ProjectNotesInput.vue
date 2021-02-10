6e71e283597843ab0fa190b314de4c6c58c283bb
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2021-02-11 01:04:27 +0800

    Create ProjectNotesInput.vue

---
+++ frontend/components/input/project/ProjectNotesInput.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-textarea
+    v-model="data"
+    :rules="validation"
+    :label="label"
+    :readonly="readonly"
+    auto-grow
+    />
+</template>
+
+<script>
+
+export default {
+  name: 'ProjectNotesInput',
+
+  props: {
+    value: {
+      type: String,
+      default: ''
+    },
+    readonly: {
+      type: Boolean,
+      default: false
+    },
+    label: {
+      type: String,
+      default: 'Project Notes'
+    }
+  },
+
+  data () {
+    return {
+      data: this.value,
+      validation: [v => !!v || 'Project Notes is required']
+    }
+  },
+
+  watch: {
+    data: {
+      immediate: true,
+      handler (newValue, oldValue) {
+        this.$emit('input', newValue)
+      }
+    },
+    value: {
+      handler (newValue, oldValue) {
+        this.data = this.value
+      }
+    }
+  }
+}
+
+</script>
