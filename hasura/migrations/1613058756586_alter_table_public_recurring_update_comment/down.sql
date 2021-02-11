ae4cb6d1c45fa194a34c17b8bc537507c7c3fd36
Glen See <54426545+glensee@users.noreply.github.com>
2021-02-12 00:02:45 +0800

    Added event_pwas & vols

---
+++ hasura/migrations/1613058756586_alter_table_public_recurring_update_comment/down.sql
@@ -0,0 +1,1 @@
+COMMENT ON TABLE "public"."recurring" IS NULL;
---
+++ hasura/migrations/1613058756586_alter_table_public_recurring_update_comment/up.sql
@@ -0,0 +1,1 @@
+COMMENT ON TABLE "public"."recurring" IS E'day: 0-6, Mon-Sun week: 1,2,3,4,-1 First Week - Last Week of the Month Frequency: Weekly, Biweekly, Monthly';
