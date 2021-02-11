f136df633fa9e223abed00fc38fe44e0348151cb
CliffenLee <cliffen.lee.2018@sis.smu.edu.sg>
2021-02-11 15:09:10 +0800

    fixed token refreshing issue with create folders and created base code for viewFolders

---
+++ backend/googleapi/listFiles/lambda_function.py
@@ -0,0 +1,25 @@
+import json
+import os
+from Google import Create_Service
+
+CLIENT_SECRET_FILE = "aphasiasg.json"
+API_NAME = "drive"
+API_VERSION = "v3"
+SCOPES = ["https://www.googleapis.com/auth/drive"]
+SERVICE = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+
+def lambda_handler(event, context):
+    print(event)
+    result = {}
+    statusCode = 500
+    drive_id = os.environ['drive_id']
+
+
+    return {
+        "statusCode": statusCode,
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+        },
+        "body": json.dumps(result)
+    }
