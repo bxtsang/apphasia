8a4ecd1d84726b5507d05e66608f5cf142c3cc5f
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2021-02-18 04:38:44 +0800

    create tabs

--- frontend/components/project/IndividualProjectView.vue
+++ frontend/components/project/IndividualProjectView.vue
@@ -2,40 +2,88 @@
   <v-row>
     <v-col>
       <v-card class="px-6 py-3">
-        <h1 class="title hover-underline">
-          <NuxtLink to="/projects">
-            Projects
-          </NuxtLink> / {{ project ? project.name : "" }}
-        </h1>
+        <ApolloQuery
+          :query="require('./../../graphql/project/GetSingleProject.graphql')"
+          :variables="{ id: projectId }"
+        >
+          <template v-slot="{ result: { error, data }, isLoading }">
+            <div v-if="isLoading" class="d-flex justify-center">
+              <v-progress-circular
+                :size="50"
+                color = "primary"
+                indeterminate
+              />
+            </div>
+
+            <!-- Error -->
+            <div v-else-if="error">
+              An error occurred
+            </div>
+
+            <div v-else-if="data && data.projects_by_pk">
+              <v-container class="pa-0 ma-0">
+                <v-row>
+                  <v-col>
+                    <h1 class="title hover-underline">
+                      <NuxtLink to="/projects">
+                        Projects
+                      </NuxtLink> / {{ data.projects_by_pk.title }}
+                    </h1>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col>
+                    <v-tabs>
+                      <v-tab
+                        v-for="(tab, index) in tabs"
+                        :key="tab"
+                        @click="selectedTab = index"
+                      >
+                        {{ tab }}
+                      </v-tab>
+                    </v-tabs>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col>
+                    <transition-group name="fade" mode="out-in">
+                      <div key="0" v-if="selectedTab == 0">Slide 1</div>
+                      <div key="1" v-else-if="selectedTab == 1">Slide 2</div>
+                      <div key="2" v-else>Slide 3</div>
+                    </transition-group>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </div>
+          </template>
+        </ApolloQuery>
+        <!--
         <v-row>
           <v-col lg="6" md="6" sm="12">
             <ProjectDetails :isLoading="isLoading" :project="project" />
-          </v-col>
-          <v-col lg="6" md="6" sm="12">
-            <ProjectTasks />
           </v-col>
         </v-row>
         <v-row>
           <v-col>
             <ResourceDirectory />
           </v-col>
-        </v-row>
+        </v-row> -->
       </v-card>
     </v-col>
   </v-row>
 </template>
 <script>
-import ProjectDetails from './ProjectDetails'
-import ProjectTasks from './ProjectTasks'
-import ResourceDirectory from './ResourceDirectory'
 
 export default {
-  components: { ProjectDetails, ProjectTasks, ResourceDirectory },
   data () {
     return {
-      isLoading: true,
       projectId: this.$route.query.id,
-      project: null
+      selectedTab: 0,
+      tabs: [
+        'Details',
+        'Resources',
+        'Events'
+      ]
     }
   }
 }
@@ -47,5 +95,11 @@ export default {
 }
 .hover-underline > a:hover {
   text-decoration: underline !important;
+}
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .3s ease;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
