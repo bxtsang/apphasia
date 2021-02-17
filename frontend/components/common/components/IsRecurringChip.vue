00a7c7210068dced4c323f49d10337a8912b9952
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2021-02-18 02:46:36 +0800

    update recurring chip

--- frontend/components/common/components/IsRecurringChip.vue
+++ frontend/components/common/components/IsRecurringChip.vue
@@ -1,5 +1,7 @@
 <template>
-  <v-chip color="primary"><v-icon>mdi-repeat</v-icon>{{ value ? ' Yes' : ' No' }}</v-chip>
+  <v-chip color="primary">
+    <v-icon v-if="value">mdi-repeat</v-icon><v-icon v-else>mdi-repeat-off</v-icon>{{ value ? ' Yes' : ' No' }}
+  </v-chip>
 </template>
 <script>
 
