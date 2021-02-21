ac1db6f59ee3e63ad9cf4ecac1a701e768bc6e0c
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2021-02-21 21:05:30 +0800

    create event list view

---
+++ frontend/components/project/events/EventsView.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container class="pa-0 ma-0" fluid>
+    <v-tabs-items v-model="tab">
+      <v-tab-item>
+        <ListingQuery :resourceType="resourceType"/>
+        <v-btn @click="tab = 1">next</v-btn>
+      </v-tab-item>
+      <v-tab-item>
+        <SingleEventView @home="tab = 0"/>
+      </v-tab-item>
+    </v-tabs-items>
+  </v-container>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      tab: 0,
+      singleEvent: -1,
+      resourceType: 'events'
+    }
+  }
+}
+</script>
