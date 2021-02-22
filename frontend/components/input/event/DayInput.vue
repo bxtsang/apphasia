96359a0bfe50f03a70ca7eb1db6ffec036a8dedc
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2021-02-23 02:41:22 +0800

    Create DayInput.vue

---
+++ frontend/components/input/event/DayInput.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-select
+    v-model="data"
+    :items="DAY"
+    :label="label"
+    :rules="validation"
+  />
+</template>
+
+<script>
+import { DAY } from './../../../assets/data'
+
+export default {
+  name: 'DayInput',
+
+  props: {
+    value: {
+      type: String,
+      default: 'None'
+    },
+    required: {
+      type: Boolean,
+      default: false
+    },
+    label: {
+      type: String,
+      default: 'Repeat'
+    }
+  },
+  data () {
+    return {
+      data: this.value,
+      DAY,
+      validation: [...(this.required ? [v => !!v || 'Day is required'] : [])]
+    }
+  },
+
+  watch: {
+    data: {
+      immediate: true,
+      handler (newValue, oldValue) {
+        this.$emit('input', newValue)
+      }
+    },
+    value: {
+      handler (newValue, oldValue) {
+        this.data = this.value
+      }
+    }
+  }
+}
+
+</script>
