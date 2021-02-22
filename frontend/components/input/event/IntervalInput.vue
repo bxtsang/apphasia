0130c4dadcb990bf97c6cb38f8ecb03d1f5d0ac1
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2021-02-23 02:41:19 +0800

    Create IntervalInput.vue

---
+++ frontend/components/input/event/IntervalInput.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-select
+    v-model="data"
+    :items="INTERVAL_OPTIONS"
+    :label="label"
+    :rules="validation"
+  />
+</template>
+
+<script>
+
+export default {
+  name: 'IntervalInput',
+
+  props: {
+    value: {
+      type: String,
+      default: 'None'
+    },
+    required: {
+      type: Boolean,
+      default: false
+    },
+    label: {
+      type: String,
+      default: 'Repeat'
+    },
+    type: {
+      type: String,
+      default: 'Day'
+    }
+  },
+  data () {
+    return {
+      data: this.value,
+      INTERVAL_OPTIONS: [
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        10,
+        11,
+        12
+      ],
+      validation: [...(this.required ? [v => !!v || 'Interval is required'] : [])]
+    }
+  },
+
+  watch: {
+    data: {
+      immediate: true,
+      handler (newValue, oldValue) {
+        this.$emit('input', newValue)
+      }
+    },
+    value: {
+      handler (newValue, oldValue) {
+        this.data = this.value
+      }
+    }
+  }
+}
+
+</script>
