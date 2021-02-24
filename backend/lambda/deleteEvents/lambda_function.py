9974457227b5c682e53b630e19bb6a1cc755f4cf
Cliffenlee <54625059+Cliffenlee@users.noreply.github.com>
2021-02-24 18:30:19 +0800

    Created DeleteEvents hasura actions (#234)
    * created separate logic for delete all events
    
    * deleted git ignore for deployment package
    
    * implemented deleteEvents function
    
    * added graphql file for deleteEvents

--- backend/lambda/deleteEvents/lambda_function.py
+++ backend/lambda/deleteEvents/lambda_function.py
@@ -8,23 +8,20 @@ clientSecret = boto3.client('secretsmanager')
 hasura_url = os.environ['HASURA_URI']
 
 
-def lambda_handler(event, context): 
+def lambda_handler(event, context):
     target = ''
     date = ''
     result = {}
     statusCode = 500
     event = json.loads(event['body'])['input']['eventData']
     hasura_secret = get_secret()
-    print(event['recurrence_id'])
-    print(event['event_id'])
-    print(event['date'])
 
     try:
+        #delete event and onwards
         if (event['date'] != None):
-            print('recurrence')
             recurrence_id = event['recurrence_id']
             date = event['date']
-            target = 'reccurrence'
+            target = f'all events of recurrence id {recurrence_id} from {date} and onwards'
             query = f"""
             mutation {{
                 delete_events ( where: {{ _and: {{ recurr_id: {{ _eq: { recurrence_id } }}, date: {{ _gte: "{ date }" }} }} }}
@@ -36,11 +33,10 @@ def lambda_handler(event, context):
                 }}
                 }}
             """
-        else:
-            print('event')
-            target = 'event'
+        #delete one event
+        elif (event['event_id'] != None):
             event_id = event['event_id']
-            print(event_id)
+            target = f'event of event id {event_id}'
             query = f"""
             mutation {{
                 delete_events(
@@ -53,6 +49,34 @@ def lambda_handler(event, context):
                 }}
             }}
             """
+        #delete ALL events
+        elif (event['recurrence_id'] != None):
+            recurrence_id = event['recurrence_id']
+            target = f'all events of recurrence id {recurrence_id}'
+            query = f"""
+            mutation {{
+                delete_events(
+                    where: {{ recurr_id: {{_eq: {recurrence_id} }} }}
+                ) {{
+                    affected_rows
+                    returning {{
+                    id
+                    }}
+                }}
+            }}
+            """
+
+        else:
+            result['status'] = "error"
+            result['message'] = "all fields are null "
+            return {
+            "statusCode": 400,
+            "headers": {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*"
+            },
+            "body": json.dumps(result)
+            }
 
         headers = {
             "Content-Type": "application/json",
@@ -72,6 +96,7 @@ def lambda_handler(event, context):
             result['code'] = r.status_code
             result['status'] = "error"
             result['message'] = "failed to delete " + target
+
 
     except Exception as e:
         result['code'] = 400
