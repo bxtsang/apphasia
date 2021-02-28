cc6f5ee01f10630ac0e7f84953cf6d8e24b0d798
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2021-02-28 22:39:24 +0800

    fix input bug for day and week inputs for events

--- frontend/components/input/event/WeekInput.vue
+++ frontend/components/input/event/WeekInput.vue
@@ -24,8 +24,8 @@ export default {
 
   props: {
     value: {
-      type: String,
-      default: 'None'
+      type: Number,
+      default: -1
     },
     required: {
       type: Boolean,
@@ -40,7 +40,7 @@ export default {
     return {
       data: this.value,
       WEEK,
-      validation: [...(this.required ? [v => (v > 0 & v < 5) || 'Week is required'] : [])]
+      validation: [...(this.required ? [v => (v > 0 && v < 5) || 'Week is required'] : [])]
     }
   },
 
