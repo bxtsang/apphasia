4e0182ea2aa4b55f9a8ca91a7dea20d0fe601a07
Glen See <54426545+glensee@users.noreply.github.com>
2021-02-28 11:57:21 +0800

    Removed some foreign keys actions, will add them back depending on business logic

---
+++ hasura/migrations/1614440252262_set_fk_public_staff_supervisors_supervisor_id/down.sql
@@ -0,0 +1,7 @@
+alter table "public"."staff_supervisors" drop constraint "staff_supervisors_supervisor_id_fkey",
+          add constraint "staff_supervisors_supervisor_id_fkey"
+          foreign key ("supervisor_id")
+          references "public"."staffs"
+          ("id")
+          on update restrict
+          on delete restrict;
---
+++ hasura/migrations/1614440309375_set_fk_public_test_staff_id/down.sql
@@ -0,0 +1,7 @@
+alter table "public"."test" drop constraint "test_staff_id_fkey",
+          add constraint "test_staff_id_fkey"
+          foreign key ("staff_id")
+          references "public"."staffs"
+          ("id")
+          on update restrict
+          on delete restrict;
