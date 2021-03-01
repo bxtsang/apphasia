dc85b34659e60a27b136be760329ac8bc1389863
Cliffenlee <cliffen.lee.2018@sis.smu.edu.sg>
2021-03-01 18:08:43 +0800

    added password rules to change password

--- frontend/components/settings/ChangePassword.vue
+++ frontend/components/settings/ChangePassword.vue
@@ -1,5 +1,10 @@
 <template>
-  <v-form ref="passwordform" v-model="passwordFormValid" class="mt-6 d-flex flex-column" @submit.prevent="changePassword">
+  <v-form
+    ref="passwordform"
+    v-model="passwordFormValid"
+    class="mt-6 d-flex flex-column"
+    @submit.prevent="changePassword"
+  >
     <v-text-field
       v-model="password.current"
       :append-icon="showCurrentPassword ? 'mdi-eye' : 'mdi-eye-off'"
@@ -16,7 +21,12 @@
       label="New Password"
       @click:append="showNewPassword = !showNewPassword"
     />
-    <v-btn color="primary" class="my-3 align-self-end" type="submit" :loading="isSubmitting">
+    <v-btn
+      color="primary"
+      class="my-3 align-self-end"
+      type="submit"
+      :loading="isSubmitting"
+    >
       Save
     </v-btn>
   </v-form>
@@ -33,14 +43,23 @@ export default {
         current: '',
         new: ''
       },
-      passwordRules: [v => !!v || 'Field Required']
+      passwordRules: [
+        v => !!v || 'Password is required',
+        v => (v && v.length >= 8) || 'Password must have 8 or more characters',
+        v => /(?=.*[A-Z])/.test(v) || 'Must have one uppercase character',
+        v => /(?=.*[a-z])/.test(v) || 'Must have one lower character',
+        v => /(?=.*\d)/.test(v) || 'Must have one number',
+        v => /([!@$%])/.test(v) || 'Must have one special character [!@#$%]'
+      ]
     }
   },
   methods: {
     changePassword () {
       if (this.$refs.passwordform.validate()) {
         this.isSubmitting = true
-        const accessToken = localStorage.getItem(`auth.CognitoIdentityServiceProvider.${this.$auth.strategies.cognito.options.clientId}.${this.$auth.user.sub}.accessToken`)
+        const accessToken = localStorage.getItem(
+          `auth.CognitoIdentityServiceProvider.${this.$auth.strategies.cognito.options.clientId}.${this.$auth.user.sub}.accessToken`
+        )
         const postBody = {
           access_token: accessToken,
           proposed_password: this.password.new,
@@ -49,19 +68,29 @@ export default {
         const postHeader = {
           'Content-Type': 'application/json'
         }
-        this.$axios.post(
-          'https://jqi5g2tgj2.execute-api.ap-southeast-1.amazonaws.com/dev',
-          JSON.stringify(postBody),
-          { postHeader }
-        ).then((response) => {
-          this.$store.commit('notification/newNotification', ['Password has been updated', 'success'])
-        }).catch((error) => {
-          console.log('error: ' + error.response.data.error)
-          this.$store.commit('notification/newNotification', [error.response.data.message, 'error'])
-        }).then(() => {
-          this.isSubmitting = false
-          this.$refs.passwordform.reset()
-        })
+        this.$axios
+          .post(
+            'https://jqi5g2tgj2.execute-api.ap-southeast-1.amazonaws.com/dev',
+            JSON.stringify(postBody),
+            { postHeader }
+          )
+          .then((response) => {
+            this.$store.commit('notification/newNotification', [
+              'Password has been updated',
+              'success'
+            ])
+          })
+          .catch((error) => {
+            console.log('error: ' + error.response.data.error)
+            this.$store.commit('notification/newNotification', [
+              error.response.data.message,
+              'error'
+            ])
+          })
+          .then(() => {
+            this.isSubmitting = false
+            this.$refs.passwordform.reset()
+          })
       }
     }
   }
