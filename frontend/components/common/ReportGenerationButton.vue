83e3beed22b139320b48dbf419ec0a59d7ce2fe8
Arix <arixgg@gmai.com>
2021-03-19 01:12:28 +0800

    Call report generation api in button and generate project report for download

--- frontend/components/common/ReportGenerationButton.vue
+++ frontend/components/common/ReportGenerationButton.vue
@@ -15,21 +15,53 @@ export default {
     className: {
       type: String,
       default: ''
+    },
+    resourceType: {
+      type: String,
+      default: 'pwas'
     }
   },
   data () {
     return {
       isLoading: false,
+      generationLink: 'https://cj00d9qd80.execute-api.ap-southeast-1.amazonaws.com/dev',
       sheets: [{ c: 2 }]
     }
   },
   methods: {
-    downloadReport () {
-      console.log(utils)
-      const wb = utils.book_new()
-      const ws = utils.json_to_sheet(this.sheets)
-      utils.book_append_sheet(wb, ws, 'Sheet 1')
-      writeFile(wb, 'report.xlsx')
+    async downloadReport () {
+      const postHeader = {
+        'Content-Type': 'application/json'
+      }
+      const postBody = {
+        resources: this.resourceType
+      }
+      await this.$axios.post(this.generationLink, JSON.stringify(postBody), { postHeader }).then((response) => {
+        switch (this.resourceType) {
+          case 'projects':
+            this.generateProjectsReport(response.data)
+            break
+          case 'volunteers':
+            break
+          default: // pwas
+            break
+        }
+      }).catch((error) => {
+        console.error(error)
+      })
+    },
+    generateProjectsReport (data) {
+      const workbook = utils.book_new()
+      const dataArray = [['Project Analytics', ''], ['Project Name', '# of events per year']]
+      for (const projectName in data) {
+        dataArray.push([projectName, data[projectName]])
+      }
+      const worksheet = utils.aoa_to_sheet(dataArray)
+      utils.book_append_sheet(workbook, worksheet, 'Sheet 1')
+      writeFile(workbook, `projects_report_${new Date().toString().substring(4, 15)}.xlsx`)
+    },
+    generateExternalPeopleReport (data) {
+
     }
   }
 }
