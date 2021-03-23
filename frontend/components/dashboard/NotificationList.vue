cc80a065b5b25a5c81d74b185a852b555108e963
Arix <arixgg@gmail.com>
2021-03-23 17:30:32 +0800

    added tab for unread and read for notification

--- frontend/components/dashboard/NotificationList.vue
+++ frontend/components/dashboard/NotificationList.vue
@@ -1,17 +1,39 @@
 <template>
   <v-card class="px-6 py-3">
-    <h1 class="title">Notifications</h1>
-    <div class="notification-wrapper">
-      <v-list>
-        <NotificationItem v-for="item in notifications" :notification="item" v-bind:key="item.id" />
-      </v-list>
+    <div class="d-flex justify-space-between align-center">
+      <h1 class="title">Notifications</h1>
+      <v-btn color="primary">Mark all as read</v-btn>
     </div>
+    <v-tabs v-model="tab">
+      <v-tab
+        v-for="option in notificationTabOptions"
+        :key="option"
+      >
+        {{ option }}
+      </v-tab>
+    </v-tabs>
+    <v-tabs-items v-model="tab">
+      <v-tab-item
+        v-for="option in notificationTabOptions"
+        :key="option"
+      >
+        <div class="notification-wrapper">
+          <v-list>
+            <NotificationItem v-for="item in notifications" :notification="item" v-bind:key="item.id" />
+          </v-list>
+        </div>
+      </v-tab-item>
+    </v-tabs-items>
   </v-card>
 </template>
 <script>
 export default {
   data () {
     return {
+      tab: 'read',
+      notificationTabOptions: [
+        'unread', 'read'
+      ],
       notifications: [
         {
           id: 1,
@@ -30,6 +52,30 @@ export default {
           type: 'delete',
           description: 'User XX has been archived',
           user: 'User 3'
+        },
+        {
+          id: 4,
+          type: 'insert',
+          description: 'Befriender assigned to person 1',
+          user: 'User 1'
+        },
+        {
+          id: 4,
+          type: 'insert',
+          description: 'Befriender assigned to person 1',
+          user: 'User 1'
+        },
+        {
+          id: 4,
+          type: 'insert',
+          description: 'Befriender assigned to person 1',
+          user: 'User 1'
+        },
+        {
+          id: 4,
+          type: 'insert',
+          description: 'Befriender assigned to person 1',
+          user: 'User 1'
         },
         {
           id: 4,
