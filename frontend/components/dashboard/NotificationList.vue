1880c7cf6b9fd3c18c412474f23e8b5a8a204502
Arix <arixgg@gmail.com>
2021-03-24 20:49:03 +0800

    Pull notification from hasura with a query

--- frontend/components/dashboard/NotificationList.vue
+++ frontend/components/dashboard/NotificationList.vue
@@ -1,62 +1,37 @@
 <template>
-  <v-list>
-    <NotificationItem v-for="item in notifications" :notification="item" v-bind:key="item.id" />
-  </v-list>
+  <ApolloQuery
+    :query="require('./../../graphql/notifications/GetNotificationOfStaffInitial.graphql')"
+    :variables="{
+      staff: $auth.user['custom:hasura_id']
+    }"
+    >
+    <template v-slot="{ result: { error, data }, isLoading }">
+      <!-- Loading -->
+      <div v-if="isLoading" class="d-flex justify-center">
+        <v-progress-circular
+          :size="50"
+          color="primary"
+          indeterminate
+          class="ma-6"
+        />
+      </div>
+
+      <!-- Error -->
+      <div v-else-if="error">An error occurred</div>
+      <div v-else-if="data">
+        {{ data.notifications[0]}}
+        <v-list>
+          <NotificationItem v-for="item in data.notifications" :notification="item" v-bind:key="item.id" />
+        </v-list>
+      </div>
+    </template>
+  </ApolloQuery>
 </template>
 <script>
+
 export default {
   data () {
     return {
-      notifications: [
-        {
-          id: 1,
-          type: 'update',
-          description: 'Chit Chat Event Updated',
-          user: 'User 1'
-        },
-        {
-          id: 2,
-          type: 'insert',
-          description: 'New Document Added',
-          user: 'User 2'
-        },
-        {
-          id: 3,
-          type: 'delete',
-          description: 'User XX has been archived',
-          user: 'User 3'
-        },
-        {
-          id: 4,
-          type: 'insert',
-          description: 'Befriender assigned to person 1',
-          user: 'User 1'
-        },
-        {
-          id: 4,
-          type: 'insert',
-          description: 'Befriender assigned to person 1',
-          user: 'User 1'
-        },
-        {
-          id: 4,
-          type: 'insert',
-          description: 'Befriender assigned to person 1',
-          user: 'User 1'
-        },
-        {
-          id: 4,
-          type: 'insert',
-          description: 'Befriender assigned to person 1',
-          user: 'User 1'
-        },
-        {
-          id: 4,
-          type: 'insert',
-          description: 'Befriender assigned to person 1',
-          user: 'User 1'
-        }
-      ]
     }
   }
 }
