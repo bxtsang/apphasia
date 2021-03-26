c4e04aaa309c7442eb53e7fdb99766e4936d9a69
bxtsang <bxtsang.2018@sis.smu.edu.sg>
2021-03-26 22:08:18 +0800

    Use ConsentInput in individual PWA view

--- frontend/components/pwa/IndividualPWAView.vue
+++ frontend/components/pwa/IndividualPWAView.vue
@@ -166,7 +166,13 @@
                     <v-select
                       :value="data.pwas_by_pk.general_info.consent"
                       :items="[{ value: true, text: 'Yes' }, { value: false, text: 'No' }]"
-                      label="Consent"
+                      label="Agreeable to receive "
+                      readonly
+                    />
+                    <ConsentInput
+                      :value="data.pwas_by_pk.general_info.consent"
+                      :required="true"
+                      input-type="select"
                       readonly
                     />
                   </v-col>
