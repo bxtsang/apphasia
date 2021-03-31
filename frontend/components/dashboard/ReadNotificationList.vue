8a145501ad98c2a9d4c4157d292bcfc2670b4a58
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2021-03-31 18:34:25 +0800

    Frontend call read notifications (#352)
    * Update GetReadNotificationsOfStaff.graphql
    
    * create read notification list with lazy loading
    
    * update notification item to display read or unread
    
    * update mark all as read button to refresh query for read list

--- frontend/components/dashboard/ReadNotificationList.vue
+++ frontend/components/dashboard/ReadNotificationList.vue
@@ -3,13 +3,13 @@
     :query="LIST_QUERY_PATHS['notification']['read']"
     :variables="{
       staff: $auth.user['custom:hasura_id'],
-      limit: fetchLimit,
-      offset
+      limit: $options.fetchLimit,
+      offset: 0
     }"
     >
-    <template v-slot="{ result: { error, data }, isLoading }">
+    <template v-slot="{ result: { error, data , loading}, isLoading, query }">
       <!-- Loading -->
-      <div v-if="isLoading" class="d-flex justify-center">
+      <div v-if="loading" class="d-flex justify-center">
         <v-progress-circular
           :size="50"
           color="primary"
@@ -22,9 +22,12 @@
       <div v-else-if="error">An error occurred</div>
       <div v-else-if="data">
         <v-list>
-          <NotificationItem v-for="item in data.notifications" :notification="item" v-bind:key="item.id" />
+          <NotificationItem v-for="item in data.notifications" :notification="item" v-bind:key="item.id" :query="query"/>
+          <v-list-item v-if="data.notifications.length < data.notifications_aggregate.aggregate.totalCount" class="d-flex justify-center">
+            <v-btn @click="fetchMore(query)" :loading="isLoading === 1" color="primary">Load more</v-btn>
+          </v-list-item>
           <v-list-item v-if="data.notifications && data.notifications.length === 0" class="d-flex justify-center">
-            No unread notifications
+            No notifications
           </v-list-item>
         </v-list>
       </div>
@@ -35,11 +38,31 @@
 <script>
 import { LIST_QUERY_PATHS } from './../../assets/data.js'
 export default {
+  fetchLimit: 5,
   data () {
     return {
       LIST_QUERY_PATHS,
-      fetchLimit: 5,
       offset: 0
+    }
+  },
+  methods: {
+    async fetchMore (query) {
+      this.offset += this.$options.fetchLimit
+      await query.fetchMore({
+        variables: {
+          staff: this.$auth.user['custom:hasura_id'],
+          limit: this.$options.fetchLimit,
+          offset: this.offset
+        },
+        updateQuery: (prev, { fetchMoreResult }) => {
+          console.log(prev)
+          console.log(fetchMoreResult)
+          return Object.assign({}, prev, {
+            notifications: [...prev.notifications, ...fetchMoreResult.notifications],
+            notifications_aggregate: fetchMoreResult.notifications_aggregate
+          })
+        }
+      })
     }
   }
 }
