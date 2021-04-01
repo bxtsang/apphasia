a76b3b81dc2b84198314ac20d5bdc3833f2a6899
CliffenLee <cliffen.lee.2018@sis.smu.edu.sg>
2021-04-01 17:42:23 +0800

    Changed verify email and send code api link to prod

--- frontend/pages/verifyEmail.vue
+++ frontend/pages/verifyEmail.vue
@@ -56,10 +56,11 @@ export default {
         access_token: this.accessToken
       }
       const postHeader = {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        Authorization: this.accessToken
       }
       this.loading = true
-      await this.$axios.post('https://3swx2qnvu9.execute-api.ap-southeast-1.amazonaws.com/dev', JSON.stringify(postBody), { postHeader }).then((resp) => {
+      await this.$axios.post('https://api.apphasia.cf/sendemail', JSON.stringify(postBody), { postHeader }).then((resp) => {
         console.log(resp)
         if (resp.data.status === 'success') {
           this.codeSent()
@@ -82,10 +83,11 @@ export default {
         email: this.email
       }
       const postHeader = {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        Authorization: this.accessToken
       }
       this.loading = true
-      await this.$axios.post('https://65vbyychn5.execute-api.ap-southeast-1.amazonaws.com/dev', JSON.stringify(postBody), { postHeader }).then((resp) => {
+      await this.$axios.post('https://api.apphasia.cf/verifyemail', JSON.stringify(postBody), { postHeader }).then((resp) => {
         console.log(resp)
         if (resp.data.status === 'success') {
           this.$store.$auth.$state.user.email_verified = 'true'
