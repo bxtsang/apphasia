ff417cd40bb5fe523b023fb6473d5e093e709e25
Tsang Bao Xian <54926274+bxtsang@users.noreply.github.com>
2021-04-16 19:05:42 +0800

    Project registration (#381)
    * Create and use RegistrationAvailabilityInput
    
    * Show registration availability on ProjectIndividualView
    
    * Fix formatting
    
    * Filter projects on registration
    
    * Update display_xxx_registration when submitting

---
+++ frontend/components/input/project/RegistrationAvailabilityInput.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-switch
+    v-model="data"
+    :label="`Available for ${resourceType}: ${allowed}`"
+    :readonly="readonly"
+  />
+</template>
+
+<script>
+export default {
+  name: 'RegistrationAvailabilityInput',
+
+  props: {
+    value: {
+      type: Boolean,
+      default: false
+    },
+    resourceType: {
+      type: String,
+      default: null
+    },
+    readonly: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  data () {
+    return {
+      data: this.value
+    }
+  },
+
+  computed: {
+    allowed () {
+      if (this.value) {
+        return 'Yes'
+      } return 'No'
+    }
+  },
+
+  watch: {
+    data: {
+      immediate: true,
+      handler (newVal) {
+        this.$emit('input', newVal)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
