76f347ad1854cc1d2fba9e0a6e135b7bc17aae50
Arix <arixgg@gmail.com>
2021-04-20 16:12:08 +0800

    change NOK add button style

--- frontend/components/input/NOKInput.vue
+++ frontend/components/input/NOKInput.vue
@@ -50,7 +50,7 @@
 
     <v-row class="mt-3" v-if="data.length < 3">
       <v-col cols="12" class="py-0 d-flex justify-center">
-        <v-btn color="warning" class="my-3" @click="addNOK">Add Caregiver</v-btn>
+        <v-btn color="primary" outlined class="my-3" @click="addNOK">Add Caregiver</v-btn>
       </v-col>
     </v-row>
 
