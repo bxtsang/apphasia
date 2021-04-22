efc30cc3c72f944366fb9090104b68e65cdece0f
Cliffenlee <cliffen.lee.2018@sis.smu.edu.sg>
2021-04-22 23:19:28 +0800

    fixed alignment for project folder

--- frontend/components/project/resources/Resource.vue
+++ frontend/components/project/resources/Resource.vue
@@ -3,7 +3,7 @@
     <!-- Item Card Folder-->
     <v-card
       v-if="resourceType === 'folder'"
-      class="d-flex align-center clickable"
+      class="ma-2 d-flex align-center clickable"
       outlined
       width="270"
       @contextmenu="show"
